f7642cdbc62ecfa3cafd1cca89b7deb2f22b9721
Manuel Bieh <github@manuelbieh.de>
2014-01-15 13:37:57 +0100

    Handlebars Draft

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
-        app: '../app'
+        app: '../app',
+        handlebars: 'handlebars-1.3.0'
+    },
+    shim: {
+		'handlebars': {
+			'exports': 'Handlebars'
+		}
     }
+});
+
+require( ["jquery", "handlebars"], function($, hbs) {
+
+	var tpl = hbs.compile($('[data-tpl="password-list"]').html());
+	console.log(tpl({
+		items: [{
+			url: 'foo',
+			notes: 'ntoes',
+			encrypted: 'enc',
+			resolved: 'res',
+			title: 'TITLE'
+		}]
+	}));
+
 });
